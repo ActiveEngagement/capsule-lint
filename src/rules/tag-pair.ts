afe76ba066bc95191f5f4add9daa7d1148977bf5
Justin Kimbrell <jkimbrell@actengag.com>
2023-12-19 14:16:11 -0700

    fix: issues with tag-pair so we can better lint closing tags

--- src/rules/tag-pair.ts
+++ src/rules/tag-pair.ts
@@ -1,22 +1,23 @@
+import { Block } from 'htmlhint/htmlparser';
 import { Rule } from 'htmlhint/types';
 
 const rule: Rule =  {
     id: 'tag-pair',
     description: 'Tag must be paired.',
     init(parser, reporter) {
-        const stack = [];
-        const mapEmptyTags = parser.makeMap('area,base,basefont,br,col,frame,hr,img,input,isindex,link,meta,param,embed,track,command,source,keygen,wbr');
+        const stack: Block[] = [];
+        const mapEmptyTags = parser.makeMap(
+            'area,base,basefont,br,col,frame,hr,img,input,isindex,link,meta,param,embed,track,command,source,keygen,wbr'
+        );
 
         parser.addListener('tagstart', (event) => {
             const tagName = event.tagName.toLowerCase();
+            
             if (mapEmptyTags[tagName] === undefined && !event.close) {
-                stack.push({
-                    tagName: tagName,
-                    line: event.line,
-                    raw: event.raw,
-                });
+                stack.push({ tagName, ...event });
             }
         });
+
         parser.addListener('tagend', (event) => {
             const tagName = event.tagName.toLowerCase();
             
@@ -25,35 +26,73 @@ const rule: Rule =  {
             }
 
             let pos;
+
             for (pos = stack.length - 1; pos >= 0; pos--) {
                 if (stack[pos].tagName === tagName) {
                     break;
                 }
             }
+
             if (pos >= 0) {
                 const arrTags = [];
+
                 for (let i = stack.length - 1; i > pos; i--) {
                     arrTags.push(`</${stack[i].tagName}>`);
                 }
+
                 if (arrTags.length > 0) {
                     const lastEvent = stack[stack.length - 1];
-                    reporter.error(`Tag must be paired, missing: [ ${arrTags.join('')} ], start tag match failed [ ${lastEvent.raw} ] on line ${lastEvent.line}.`, event.line, event.col, this, event.raw);
+                    
+                    reporter.error(
+                        `Tag must be paired, missing: [ ${arrTags.join('')} ], start tag match failed [ ${lastEvent.raw} ] on line ${lastEvent.line}.`,
+                        event.line,
+                        event.col,
+                        this,
+                        event.raw
+                    );
                 }
+
                 stack.length = pos;
             }
             else {
-                reporter.error(`Tag must be paired, no start tag: [ ${event.raw} ]`, event.line, event.col, this, event.raw);
+                reporter.error(
+                    `Tag must be paired, no start tag: [ ${event.raw} ]`,
+                    event.line,
+                    event.col,
+                    this,
+                    event.raw
+                );
             }
         });
+
         parser.addListener('end', (event) => {
             const arrTags = [];
+
             for (let i = stack.length - 1; i >= 0; i--) {
-                arrTags.push(`</${stack[i].tagName}>`);
+                reporter.error(
+                    `Tag must be paired, missing: [ </${stack[i].tagName}> ], start tag match failed [ ${stack[i].raw} ] on line ${stack[i].line}.`,
+                    event.line, 
+                    event.col,
+                    this,
+                    stack[i].raw
+                );
+
+                // arrTags.push(`</${stack[i].tagName}>`);
             }
-            if (arrTags.length > 0) {
-                const lastEvent = stack[stack.length - 1];
-                reporter.error(`Tag must be paired, missing: [ ${arrTags.join('')} ], open tag match failed [ ${lastEvent.raw} ] on line ${lastEvent.line}.`, event.line, event.col, this, '');
-            }
+
+            // if (arrTags.length > 0) {
+            //     const lastEvent = stack[stack.length - 1];
+
+            //     console.log(stack);
+                    
+            //     reporter.error(
+            //         `Tag must be paired, missing: [ ${arrTags.join('')} ], open tag match failed [ ${lastEvent.raw} ] on line ${lastEvent.line}.`,
+            //         event.line, 
+            //         event.col,
+            //         this,
+            //         lastEvent.raw
+            //     );
+            // }
         });
     },
 };
